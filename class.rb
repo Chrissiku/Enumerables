a8e5ee991461e665ddaeb7e532aed49a3926dede
jheart-vic <adebowalevictorjheart@gmail.com>
2022-08-18 15:06:07 +0100

    Create `any?` method in `module.rb` and did it's test in `class.rb`

--- class.rb
+++ class.rb
@@ -14,3 +14,8 @@ list_test = Mylist.new(1, 2, 3, 4)
 # Test all? method
 
 list_test.all? {|e| e < 5} # => true
+
+# Test any? method
+
+list_test.any? {|e| e == 2} # => true
+list_test.any? {|e| e == 5} # => false
--- module.rb
+++ module.rb
@@ -10,4 +10,12 @@ module MyEnumerable
         end
         p myArray.length == list.length
     end
+
+    def any? 
+        myArray = []
+        list.each do |element| 
+            myArray.push(element) if yield(element)
+        end
+        puts !myArray.empty?
+    end 
 end
